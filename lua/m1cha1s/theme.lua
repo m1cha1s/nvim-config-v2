9b4c20d0596c5e89b45a29ffa33a0dfaf59070d9
m1cha1s <msulej04@gmail.com>
2024-08-01 16:41:48 +0200

    Theme tweaks.
    Added ability to change between Dark and Light mode.

--- lua/m1cha1s/theme.lua
+++ lua/m1cha1s/theme.lua
@@ -1,5 +1,18 @@
+local DARK  = true
+local WHITE = false
+
+local theme_mode = WHITE
+
+local function SetTheme()
+    if not theme_mode then
+        return "latte"
+    else
+        return "mocha"
+    end
+end
+
 require("catppuccin").setup({
-	flavour="mocha",
+	flavour=SetTheme(),
 	transparent_background=true,
 })
 
